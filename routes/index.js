ce1dea2ea7138ce843f2cba9d9b5e48567e44762
ewellyuan <ewellyuan@live.com>
2018-09-10 15:59:15 +0800

    init express

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/post', function(req, res, next) {
+  var data = req.param('data');
+  if(data) {
+    res.send('{"S":"'+data+'"}');
+  } else {
+    res.send('{"errcode":"100"}');
+  }
+  
+});
+
+module.exports = router;
