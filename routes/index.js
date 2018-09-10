6ee688b15e43460b85ad4ce096ad2fbb756ca8fd
ewellyuan <ewellyuan@live.com>
2018-09-10 19:10:24 +0800

    add service name for redis ip

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const redis = require('redis')
+
+const client = redis.createClient(6379, 'string-service-redis')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +12,23 @@ router.get('/', function(req, res, next) {
 router.post('/post', function(req, res, next) {
   var data = req.param('data');
   if(data) {
+    client.set('data', data);
     res.send('{"S":"'+data+'"}');
   } else {
     res.send('{"errcode":"100"}');
   }
+  
+});
+
+router.post('/getredis', function(req, res, next) {
+  client.get('data', function(err, value){
+    if(value) {
+      res.send('{"RS":"'+value+'"}');
+    } else {
+      res.send('{"errcode":"200"}');
+    }
+  });
+  
   
 });
 
