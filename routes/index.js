94c1a44747fb68935509c82a5a07dc813d80bce9
ewellyuan <ewellyuan@live.com>
2018-09-11 09:31:08 +0800

    Use service name instead of host ip

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const redis = require('redis')
 
-const client = redis.createClient(6379, 'string-service-redis')
+const client = redis.createClient(6379, 'service-string-redis')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
